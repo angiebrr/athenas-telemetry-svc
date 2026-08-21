a26ce7b16edc45e502ae2ceb45623065c45e2d8c
Angie Breuer <angelahnicole@users.noreply.github.com>
2026-08-21 15:23:37 -0400

    Adding gin and setting up a dummy telemetry endpoint

--- deploy/acceptance_tests/tests/httpserver/athenas_server_test.go
+++ deploy/acceptance_tests/tests/httpserver/athenas_server_test.go
@@ -6,7 +6,9 @@ import (
 	"athenas-telemetry-acceptance-tests/internal/shared"
 )
 
-func TestGreeterServer(testCtx *testing.T) {
+// ================================================================================================
+
+func TestAthenasTelemetryServer(testCtx *testing.T) {
 	// This acceptance test takes a long time, so skip if short running tests are desired
 	if testing.Short() {
 		testCtx.Skip()
@@ -17,4 +19,8 @@ func TestGreeterServer(testCtx *testing.T) {
 		"8080/tcp",
 		"http",
 	)
+
+	// TODO: add driver for HTTP that specification tests use
+
+	// TODO: add specification test
 }
--- src/cmd/athenas/main.go
+++ src/cmd/athenas/main.go
@@ -1,9 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // ================================================================================================
 
 func main() {
 	fmt.Println("Telemetry service is running...")
+
+	// TODO: move this code somewhere else
+	// TODO: save the telemetry data, define the struct, etc. (this is just a dummy)
+	router := gin.Default()
+	router.POST("/v1/telemetry", func(ctx *gin.Context) {
+		ctx.Status(http.StatusAccepted)
+	})
+	router.Run() // listens on 0.0.0.0:8080 by default
 }
