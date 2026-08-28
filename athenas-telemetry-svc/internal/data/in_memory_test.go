da737ad635baf3eacdadb83ccbca7d359fd0fd61
Angie Breuer <angelahnicole@users.noreply.github.com>
2026-08-28 16:44:21 -0400

    Improving thread-safe tests to use wait groups + contend on the map and a specific device

--- athenas-telemetry-svc/internal/data/in_memory_test.go
+++ athenas-telemetry-svc/internal/data/in_memory_test.go
@@ -1,10 +1,11 @@
 package data_test
 
 import (
+	"sync"
 	"testing"
-	"time"
 
 	"github.com/angiebrr/athenas-telemetry-svc/internal/data"
+	"github.com/angiebrr/athenas-telemetry-svc/internal/shared"
 	"github.com/angiebrr/athenas-telemetry-svc/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -19,21 +20,14 @@ func TestInMemoryDataStore(testCtx *testing.T) {
 
 	testCtx.Run("insert and get data from store", func(subTestCtx *testing.T) {
 		// Create a sample telemetry data
-		sampleData := models.Telemetry{
-			DeviceID:  "device123",
-			Timestamp: time.Now().Unix(),
-			Metrics: []models.MetricReading{
-				{Name: "temperature", Value: 25.5},
-				{Name: "humidity", Value: 60.0},
-			},
-		}
+		sampleData := shared.ValidTelemetry()
 
 		// Insert the sample data into the store
 		err := store.Insert(sampleData)
 		require.NoError(subTestCtx, err)
 
 		// Retrieve the data by device ID
-		retrievedData, err := store.GetByDeviceID("device123")
+		retrievedData, err := store.GetByDeviceID(sampleData.DeviceID)
 		require.NoError(subTestCtx, err)
 
 		// Verify that the retrieved data matches the inserted data
@@ -55,20 +49,9 @@ func TestInMemoryDataStore(testCtx *testing.T) {
 
 	testCtx.Run("insert multiple data entries for the same device ID", func(subTestCtx *testing.T) {
 		// Create multiple sample telemetry data entries for the same device ID
-		sampleData1 := models.Telemetry{
-			DeviceID:  "device456",
-			Timestamp: time.Now().Unix(),
-			Metrics: []models.MetricReading{
-				{Name: "temperature", Value: 22.0},
-			},
-		}
-		sampleData2 := models.Telemetry{
-			DeviceID:  "device456",
-			Timestamp: time.Now().Add(1 * time.Minute).Unix(),
-			Metrics: []models.MetricReading{
-				{Name: "temperature", Value: 23.0},
-			},
-		}
+		sampleData1 := shared.ValidTelemetry()
+		sampleData2 := shared.ValidTelemetry()
+		sampleData2.DeviceID = sampleData1.DeviceID
 
 		// Insert both sample data entries into the store
 		err := store.Insert(sampleData1)
@@ -77,7 +60,7 @@ func TestInMemoryDataStore(testCtx *testing.T) {
 		require.NoError(subTestCtx, err)
 
 		// Retrieve the data by device ID
-		retrievedData, err := store.GetByDeviceID("device456")
+		retrievedData, err := store.GetByDeviceID(sampleData1.DeviceID)
 		require.NoError(subTestCtx, err)
 
 		// Verify that both entries are retrieved
@@ -87,36 +70,56 @@ func TestInMemoryDataStore(testCtx *testing.T) {
 	})
 
 	testCtx.Run("make multiple inserts concurrently", func(subTestCtx *testing.T) {
-		// Create a sample telemetry data
-		deviceID := "device789"
-		sampleData := models.Telemetry{
-			DeviceID:  deviceID,
-			Timestamp: time.Now().Unix(),
-			Metrics: []models.MetricReading{
-				{Name: "temperature", Value: 20.0},
-			},
-		}
+		contendedData := shared.ValidTelemetry()
 
-		// Insert the sample data concurrently
-		numInserts := 100
-		errCh := make(chan error, numInserts)
-		for range numInserts {
-			go func() {
-				errCh <- store.Insert(sampleData)
-			}()
-		}
+		// Insert and read the sample data concurrently
+		numGoRoutines := 100
+		var allOpsWg, gateWg sync.WaitGroup
+		gateWg.Add(1)
+		for range numGoRoutines {
+			allOpsWg.Go(func() {
+				gateWg.Wait() // wait for all go-routines to be added
 
-		// Wait for all inserts to complete and check for errors
-		for range numInserts {
-			err := <-errCh
-			require.NoError(subTestCtx, err)
-		}
+				// insert and read data for new device (contend on map)
+				newData := shared.ValidTelemetry()
+				insertGetAndIterate(subTestCtx, newData, store)
 
-		// Retrieve the data by device ID
-		retrievedData, err := store.GetByDeviceID(deviceID)
+				// insert and read same device over and over (contend on map + same device array)
+				insertGetAndIterate(subTestCtx, contendedData, store)
+			})
+		}
+		gateWg.Done()   // signal go-routines to run
+		allOpsWg.Wait() // wait until they are done
+
+		// Verify the contended device has the number of expected telemetry datapoints
+		receivedData, err := store.GetByDeviceID(contendedData.DeviceID)
 		require.NoError(subTestCtx, err)
-
-		// Verify that all entries are retrieved
-		require.Len(subTestCtx, retrievedData, numInserts)
+		assert.Len(
+			subTestCtx,
+			receivedData,
+			numGoRoutines,
+			"contended device doesn't have the expected number of datapoints",
+		)
 	})
+}
+
+func insertGetAndIterate(testCtx *testing.T, newData models.Telemetry, store data.TelemetryDataStorer) {
+	testCtx.Helper()
+
+	// Insert the data and make sure it exists
+	// Note that we use "assert" instead of "require" so the go-routine doesn't silently die
+	err := store.Insert(newData)
+	if ok := assert.NoError(testCtx, err); !ok {
+		return
+	}
+	results, err := store.GetByDeviceID(newData.DeviceID)
+	if ok := assert.NoError(testCtx, err); !ok {
+		return
+	}
+
+	// Read data from slice to test thread-safe reads and make sure we aren't leaking backing data
+	// that isn't thread-safe
+	for _, currData := range results {
+		assert.Equal(testCtx, newData.DeviceID, currData.DeviceID)
+	}
 }
