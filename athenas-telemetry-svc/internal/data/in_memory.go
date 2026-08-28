92beadbf36a1575bd8929240c3fd18f7148bb0e0
Angie Breuer <angelahnicole@users.noreply.github.com>
2026-08-28 14:41:20 -0400

    Basic implementation of an in-memory data store (not thread safe)

--- athenas-telemetry-svc/internal/data/in_memory.go
+++ athenas-telemetry-svc/internal/data/in_memory.go
@@ -4,17 +4,35 @@ import "github.com/angiebrr/athenas-telemetry-svc/models"
 
 // ================================================================================================
 
+type DataNotFoundError struct {
+	DeviceID string
+}
+
+func (rErr *DataNotFoundError) Error() string {
+	return "data not found for device: " + rErr.DeviceID
+}
+
+// ------------------------------------------------------------------------------------------------
+
 type InMemoryDataStore struct {
+	data map[string][]models.Telemetry
 }
 
 func NewInMemoryDataStore() TelemetryDataStorer {
-	return &InMemoryDataStore{}
+	return &InMemoryDataStore{
+		data: make(map[string][]models.Telemetry),
+	}
 }
 
 func (rStore *InMemoryDataStore) Insert(data models.Telemetry) error {
+	rStore.data[data.DeviceID] = append(rStore.data[data.DeviceID], data)
 	return nil
 }
 
 func (rStore *InMemoryDataStore) GetByDeviceID(deviceID string) ([]models.Telemetry, error) {
-	return nil, nil
+	results, exists := rStore.data[deviceID]
+	if !exists {
+		return nil, &DataNotFoundError{DeviceID: deviceID}
+	}
+	return results, nil
 }
